00f4ef9f0bf43bdb57d07da40a81de727a5c8062
adairrohling <81537071+adairrohling@users.noreply.github.com>
2023-12-10 12:39:17 +0000

    fix: nextconfig allowdOrigins

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 
 export default function Home() {
-  async function saveCourse(formData: FormData){
+  async function saveCourse(formData: FormData) {
     "use server"
     console.log("teste")
   }
   return (
     <div>
       <form>
-                <button formAction={saveCourse} className="">Salvar</button>
-            </form>
+        <button formAction={saveCourse} className="">Salvar</button>
+      </form>
     </div>
-   
+
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["xxxx.com", "localhost:3000"],
+      },
+    },
+  }
+//  experimental.serverActions.allowedOrigins: ["https://nextjs-14-0-2-bug-report-4ea064a57303.herokuapp.com"]
+//  serverActions: { allowedOrigins: ["xxxx.com", "localhost:3001"], }
+// from https://github.com/vercel/next.js/issues/58295
 
 module.exports = nextConfig
